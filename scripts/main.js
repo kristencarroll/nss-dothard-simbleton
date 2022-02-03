9dbec33274d80868b2d9463011f256c38b95c44f
Kristen Carroll <carroll.kristen@gmail.com>
2022-02-03 13:29:35 -0600

    removed h1s from main

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,9 @@ const contentTarget = document.querySelector(".businesses")
 
 let applicationHTML =
     `<section class="businesses">
-        <h1>Active Businesses</h1>
+       
             ${BusinessList()}
-        <h1>New York Businesses</h1>
+        
             ${NewYorkBusinessList()}
         
         
