4a9396ea9e09736d61bb4e22ef6b5ae63856f64a
Kristen Carroll <carroll.kristen@gmail.com>
2022-02-04 11:59:27 -0600

    added purchasing agents

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -2,6 +2,8 @@ import { getBusinesses } from "./database.js";
 import { Business } from "./Business.js"
 import { getNewYorkBusinessArray } from "./database.js";
 import { NewYorkBusiness} from "./Business.js";
+import { getPurchasingAgents } from "./database.js";
+import { PurchasingAgents } from "./Business.js"
 
 const contentTarget = document.querySelector(".businesses")
 
@@ -21,12 +23,29 @@ const newYorkArticle = document.querySelector(".businessList--newYork")
 
 export const NewYorkBusinessList = () => {
     const newNewYorkBusinessArray = getNewYorkBusinessArray()
+    
     newYorkArticle.innerHTML = "<h1>New York Businesses</h1>"
 
     newNewYorkBusinessArray.forEach(
         (newYorkBusinessObject) => {
             newYorkArticle.innerHTML += NewYorkBusiness(newYorkBusinessObject)
         }
+    )
+
+}
+
+const purchasingAgentArticle = document.querySelector(".agents")
+
+export const PurchasingAgentList = () => {
+    const newPurchasingAgentArray = getPurchasingAgents()
+
+    purchasingAgentArticle.innerHTML += "<h1>Purchasing Agents</h1>"
+
+    newPurchasingAgentArray.forEach(
+        (newPurchasingAgentObject) => {
+            purchasingAgentArticle.innerHTML += PurchasingAgents(newPurchasingAgentObject)
+        }
+
     )
 
 }
